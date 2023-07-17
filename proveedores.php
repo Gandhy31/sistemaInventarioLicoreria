600fee5080e4d8de24f5d1d8bce0da3f45f0ff47
Gandhy31 <gandhyarmando31@gmail.com>
2023-07-16 19:43:31 -0500

    tabla proveedores

--- proveedores.php
+++ proveedores.php
@@ -1,3 +1,18 @@
+<?php
+    include("conexion.php");
+    $conn = conectar();
+    session_start();
+    if(!isset($_SESSION['idA'])){
+      header("Location: index.php");
+    }
+    if(isset($_GET['e'])){
+      echo "<script language=\"javascript\">alert(\"Nombre de usuario o contraseña incorrectos\");</script>";
+    }
+    $sql1="SELECT*
+           FROM provedores";
+    $query1=mysqli_query($conn, $sql1);
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -240,70 +255,9 @@
                                         </tr>
                                       </thead>
                                       <tbody>
-                                        <tr>
-                                          <td>
-                                            <span class="custom-checkbox">
-                                              <input type="checkbox" id="checkbox1" name="options[]" value="1">
-                                              <label for="checkbox1"></label>
-                                            </span>
-                                          </td>
-                                          <td>Thomas Hardy</td>
-                                          <td>thomashardy@mail.com</td>
-                                          <td>89 Chiaroscuro Rd, Portland, USA</td>
-                                          <td>(171) 555-2222</td>
-                                          <td>
-                                            <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                                          </td>
-                                        </tr>
-                                        <tr>
-                                          <td>
-                                            <span class="custom-checkbox">
-                                              <input type="checkbox" id="checkbox2" name="options[]" value="1">
-                                              <label for="checkbox2"></label>
-                                            </span>
-                                          </td>
-                                          <td>Dominique Perrier</td>
-                                          <td>dominiqueperrier@mail.com</td>
-                                          <td>Obere Str. 57, Berlin, Germany</td>
-                                          <td>(313) 555-5735</td>
-                                          <td>
-                                            <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                                          </td>
-                                        </tr>
-                                        <tr>
-                                          <td>
-                                            <span class="custom-checkbox">
-                                              <input type="checkbox" id="checkbox3" name="options[]" value="1">
-                                              <label for="checkbox3"></label>
-                                            </span>
-                                          </td>
-                                          <td>Maria Anders</td>
-                                          <td>mariaanders@mail.com</td>
-                                          <td>25, rue Lauriston, Paris, France</td>
-                                          <td>(503) 555-9931</td>
-                                          <td>
-                                            <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                                          </td>
-                                        </tr>
-                                        <tr>
-                                          <td>
-                                            <span class="custom-checkbox">
-                                              <input type="checkbox" id="checkbox4" name="options[]" value="1">
-                                              <label for="checkbox4"></label>
-                                            </span>
-                                          </td>
-                                          <td>Fran Wilson</td>
-                                          <td>franwilson@mail.com</td>
-                                          <td>C/ Araquil, 67, Madrid, Spain</td>
-                                          <td>(204) 619-5731</td>
-                                          <td>
-                                            <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                                          </td>
-                                        </tr>					
+                                      <?php
+                                        while ($row = mysqli_fetch_array($query1)) {
+                                      ?>				
                                         <tr>
                                           <td>
                                             <span class="custom-checkbox">
@@ -311,15 +265,18 @@
                                               <label for="checkbox5"></label>
                                             </span>
                                           </td>
-                                          <td>Martin Blank</td>
-                                          <td>martinblank@mail.com</td>
-                                          <td>Via Monte Bianco 34, Turin, Italy</td>
-                                          <td>(480) 631-2097</td>
+                                          <td><?php echo $row['nombres'] ?></td>
+                                          <td><?php echo $row['email'] ?></td>
+                                          <td><?php echo $row['direccion'] ?></td>
+                                          <td><?php echo $row['telefono'] ?></td>
                                           <td>
                                             <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
                                             <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
                                           </td>
                                         </tr> 
+                                        <?php
+                                          }
+                                        ?>
                                       </tbody>
                                     </table>   
                                 </div>
